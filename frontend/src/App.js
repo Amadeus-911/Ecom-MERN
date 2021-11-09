5a72da10f640af0448b46953df24fd224b82f35f
Iftikhar Imrul Khan <iftikharimrul@iut-dhaka.edu>
2021-11-09 18:48:28 +0600

    Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main>
                 <Container>
-                    <HomeScreen />
+                    <Routes>
+                        <Route exact path="/" element={<HomeScreen />} />
+                    </Routes>
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,11 +6,12 @@ const Product = ({ product }) => {
     return (
         <>
             <Card className="my-3 p-3 rounded" style={{ height: '90%' }}>
-                <a href=""></a>
-                <Card.Img src={product.image} variant="top" />
+                <a href="/">
+                    <Card.Img src={product.image} variant="top" />
+                </a>
 
                 <Card.Body>
-                    <a className="nav-link" href="">
+                    <a className="nav-link" href="/">
                         <Card.Title as="div">
                             <strong>{product.name}</strong>
                         </Card.Title>
