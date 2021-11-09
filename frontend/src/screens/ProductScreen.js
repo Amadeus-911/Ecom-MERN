6e7e011ff9247b6050d4734ad20ebb996c288442
Iftikhar Imrul Khan <iftikharimrul@iut-dhaka.edu>
2021-11-09 23:27:52 +0600

    building product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Container, Row, Col, Image, ListGroup, Button } from 'react-bootstrap';
+import products from '../products';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProductScreen = () => {
+    const { id } = useParams();
+    const product = products.find((p) => p._id === id);
+
     return (
         <div>
-            <h1>Hello Product</h1>
+            <Link className="btn btn-light my-3" to="/">
+                <span>
+                    <FontAwesomeIcon icon={faArrowLeft} /> Go Back
+                </span>
+            </Link>
         </div>
     );
 };
