3ecfc546afabd36ce436f56b1659b2ed5e8b6a75
Iftikhar Imrul Khan <iftikharimrul@iut-dhaka.edu>
2021-11-11 02:05:09 +0600

    dummy users created

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bycryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@gmail.com',
+        password: bcrypt.hashSync('12345678', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'sivan',
+        email: 'sivan@gmail.com',
+        password: bcrypt.hashSync('12345678', 10),
+    },
+    {
+        name: 'muktadir',
+        email: 'muktadir@gmail.com',
+        password: bcrypt.hashSync('12345678', 10),
+    },
+];
+
+export default users;
